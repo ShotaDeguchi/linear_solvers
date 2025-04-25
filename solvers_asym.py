796780f86dcd01c5bbfbf9cefe6bfa2bc1e32f5a
ShotaDeguchi <shotadeguchio2o5@gmail.com>
2025-04-25 17:38:16 +0900

    add asymmetric (temporally)

---
+++ solvers_asym.py
@@ -0,0 +1,32 @@
+"""
+********************************************************************************
+iterative solvers for linear systems (asymmetric)
+********************************************************************************
+"""
+
+import numpy as np
+
+# ==============================================================================
+
+def ConjGradSq(A, b, x, tol=1e-9, ord=np.inf, max_iter=int(1e3)):
+    """
+    Conjugate Gradient Squared
+    """
+    raise NotImplementedError
+
+# ==============================================================================
+
+def BiConjGrad(A, b, x, tol=1e-9, ord=np.inf, max_iter=int(1e3)):
+    """
+    Bi-Conjugate Gradient
+    """
+    raise NotImplementedError
+
+# ==============================================================================
+
+def BiConjGradStab(A, b, x, tol=1e-9, ord=np.inf, max_iter=int(1e3)):
+    """
+    Bi-Conjugate Gradient Stabilized
+    """
+    raise NotImplementedError
+
